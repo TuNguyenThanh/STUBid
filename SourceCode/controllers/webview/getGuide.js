f357120e7cc70b67415a9eeb8902c0f78c80fda3
Truong Duc Minh <truongducminh@outlook.com>
2017-06-20 22:00:27 +0700

    update get guide

--- SourceCode/controllers/webview/getGuide.js
+++ SourceCode/controllers/webview/getGuide.js
@@ -1,21 +1,23 @@
-const fs = require('fs');
+const { query } = require('../../helpers/db');
 const ERROR = require('../../error.json');
-const { DIRNAME } = require('../../config');
 
 module.exports = (req,res) => {
-    fs.readFile(DIRNAME + '/public/guide.html', "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send({
-                success: false,
-                error: ERROR[500][2]
-            })
-        }
-        else {
-            res.send({
-                success: true,
-                html: data
-            })
-        }
+    let sql = `SELECT * FROM "GuideDocument"`;
+    query(sql,[])
+    .then(result => {
+        let html = '';
+        result.rows.forEach(function(element) {
+            html += `<h1>${element.title}</h1>${element.content}`
+        }, this);
+        res.send({
+            success: true,
+            html
+        })
+    })
+    .catch(reason => {
+        res.status(500).send({
+            success: false,
+            error: reason.error
+        })
     })
 }
