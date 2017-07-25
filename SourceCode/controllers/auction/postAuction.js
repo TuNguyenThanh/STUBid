40fc5ec86b0e32c0daa18e19be34887385c432db
Truong Duc Minh <truongducminh@outlook.com>
2017-07-25 19:55:06 +0700

    fix ceiling parsing

--- SourceCode/controllers/auction/postAuction.js
+++ SourceCode/controllers/auction/postAuction.js
@@ -6,10 +6,10 @@ const { insertAuction } = require('../../models/auction');
 const ERROR = require('../../error.json');
 const s3Uploader = require('../../helpers/s3');
 
-module.exports = (req,res) => {
+module.exports = (req, res) => {
     var productImages = [], token = '', auctionInfo;
     var form = new formidable.IncomingForm({ uploadDir: DIRNAME + '/public/' });
-    form.on('fileBegin', function(name, file) {
+    form.on('fileBegin', function (name, file) {
         productImages.push('product_' + file.name);
         file.path = form.uploadDir + file.name;
     });
@@ -28,11 +28,11 @@ module.exports = (req,res) => {
                 console.log(reason);
             })
     });
-    form.on('error', function(err) {
+    form.on('error', function (err) {
         console.log(err);
     });
-    new Promise(function(resolve, reject) {
-        form.parse(req, function(err, fields, files) {
+    new Promise(function (resolve, reject) {
+        form.parse(req, function (err, fields, files) {
             if (err) return reject({
                 status: 500,
                 error: ERROR[500][20],
@@ -41,57 +41,58 @@ module.exports = (req,res) => {
             return resolve(fields);
         });
     })
-    .then(fields => {
-        console.log(fields);
-        if (!(fields.token && fields.productName && fields.description && fields.categoryId
-        && fields.duration && fields.startPrice && fields.bidIncreasement))
-            return Promise.reject({
-                status: 400,
-                error: ERROR[400][0]
-            })
-        auctionInfo = fields;
-        return verify(fields.token);
-    })
-    .then(obj => {
-        if (!obj.accountId)
-            return Promise.reject({
-                status: 400,
-                error: ERROR[400][1]
-            });
-        token = refreshToken(obj);
-        let {
+        .then(fields => {
+            console.log(fields);
+            if (!(fields.token && fields.productName && fields.description && fields.categoryId
+                && fields.duration && fields.startPrice && fields.bidIncreasement))
+                return Promise.reject({
+                    status: 400,
+                    error: ERROR[400][0]
+                })
+            auctionInfo = fields;
+            return verify(fields.token);
+        })
+        .then(obj => {
+            if (!obj.accountId)
+                return Promise.reject({
+                    status: 400,
+                    error: ERROR[400][1]
+                });
+            token = refreshToken(obj);
+            let {
             productName, description, searchKey, categoryId,
-            duration, startPrice, ceilingPrice,
-            bidIncreasement, comment,
-            productReturningAddress,
-            moneyReceivingBankRefId, moneyReceivingAddress, allowedUserLevel
+                duration, startPrice, ceilingPrice,
+                bidIncreasement, comment,
+                productReturningAddress,
+                moneyReceivingBankRefId, moneyReceivingAddress, allowedUserLevel
         } = auctionInfo;
-        return insertAuction(
-            productName, description, JSON.parse('["test"]'), parseInt(categoryId),
-            productImages,
-            parseInt(duration), parseInt(startPrice), ceilingPrice?parseInt(ceilingPrice):null,
-            parseInt(bidIncreasement), comment,
-            productReturningAddress?productReturningAddress:null, obj.accountId,
-            moneyReceivingBankRefId?parseInt(moneyReceivingBankRefId):null, moneyReceivingAddress?moneyReceivingAddress:null, allowedUserLevel?parseInt(allowedUserLevel):null
-        )
-    })
-    .then(() => {
-        res.send({
-            success: true,
-            token
-        });
-    })
-    .catch(function(reason){
-        console.log(reason);
-        res.send({
-            success: false,
-            error: reason.error
+            if (ceilingPrice) ceilingPrice = parseInt(ceilingPrice);
+            return insertAuction(
+                productName, description, JSON.parse('["test"]'), parseInt(categoryId),
+                productImages,
+                parseInt(duration), parseInt(startPrice), (ceilingPrice && ceilingPrice !== NaN) ? ceilingPrice : null,
+                parseInt(bidIncreasement), comment,
+                productReturningAddress ? productReturningAddress : null, obj.accountId,
+                moneyReceivingBankRefId ? parseInt(moneyReceivingBankRefId) : null, moneyReceivingAddress ? moneyReceivingAddress : null, allowedUserLevel ? parseInt(allowedUserLevel) : null
+            )
         })
-        productImages.forEach(e => {
-            fs.unlink(form.uploadDir + e, error => {
-                if (error) console.log(error);
-                else console.log('deleted product image : ' + e);
+        .then(() => {
+            res.send({
+                success: true,
+                token
             });
         })
-    });
+        .catch(function (reason) {
+            console.log(reason);
+            res.send({
+                success: false,
+                error: reason.error
+            })
+            productImages.forEach(e => {
+                fs.unlink(form.uploadDir + e, error => {
+                    if (error) console.log(error);
+                    else console.log('deleted product image : ' + e);
+                });
+            })
+        });
 }
